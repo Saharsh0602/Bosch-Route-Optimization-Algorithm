0198d8e2faceaba6d07ba9c010489e09d8a85145
Saharsh0602 <saharshs012@gmail.com>
2021-09-18 13:14:33 +0530

    Code Files for Bosch Route Optimization

---
+++ cvrp_algorithm.py
@@ -0,0 +1,13 @@
+from cvrp_info import CVRPInfo
+
+class CVRPAlgorithm(object):
+    def __init__(self, info):
+        self.info = info
+        self.best_paths = []
+        self.best_solution = None
+
+    def random_path():
+        pass
+
+if __name__ == "__main__":
+    print("Run cvrp_runner instead")
